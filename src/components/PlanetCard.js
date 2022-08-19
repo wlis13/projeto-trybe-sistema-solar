9ffbe448d3dd20fde6f7060f043dffb13213a7aa
wlisses <wlissesfernando285@gmail.com>
2022-08-19 15:24:25 -0300

    feitas alterações no arquivo PlanetCard pois a prop img estava retornando no valor da alt planeta e não Planeta

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -5,11 +5,10 @@ class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <>
-        <div data-testid="planet-name" />
-        <p data-testid="planet-card" className="MissionsUnid">{planetName}</p>
-        <img src={ planetImage } alt={ `planeta ${planetName}` } />
-      </>
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
     );
   }
 }
