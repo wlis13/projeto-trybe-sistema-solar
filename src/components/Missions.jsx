b161ce95aa3342c496d5852f36e4c29b377175cb
wlisses <wlissesfernando285@gmail.com>
2022-08-19 14:44:30 -0300

    foi retirado o arquivo MissionCart pois eu não encontrei uma solução ainda

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import MissionCard from './MissionCard';
 import Title from './Title';
-import MissionsList from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        {MissionsList.map((itens) => (
-          <MissionCard
-            name={ itens.name }
-            year={ itens.year }
-            country={ itens.country }
-            destination={ itens.destination }
-            key={ itens.name }
-          />
-        ))}
       </div>
     );
   }
